6a3859eedae2066f0f00b563627e1bb6508e77db
mainframed <mainframed767@gmail.com>
2024-05-05 10:28:00 -0700

    adding a little mroe verbosity to the shellscripts

--- loaded.sh
+++ loaded.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+echo "* Waiting for MVS3.8j to load"
+
 while [ ! -f /home/hercules/done.txt ]
 do
   printf .
   sleep 1
 done
+
+echo "* Done!"
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+set -x
 cd $1
 ./mvs >/dev/null 2>/dev/null &
